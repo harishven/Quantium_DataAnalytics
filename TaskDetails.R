160cbb17feee32c543ff14575f37d750645a1183
harishven <60528112+harishven@users.noreply.github.com>
2020-09-17 21:11:46 -0500

    Customer Analytics & Product Affinity Analysis

---
+++ TaskDetails.R
@@ -0,0 +1,21 @@
+"
+InsideSherpa - Quantium Data Analytics Virtual Program
+
+Task 1 : Data preparation and customer analytics
+
+Conduct analysis on your client's transaction dataset and
+identify customer purchasing behaviours to generate insights 
+and provide commercial recommendations.
+
+Task 2 : Experimentation and uplift testing
+
+Extend analysis from Task 1 to help identify benchmark stores 
+that allow you to test the impact of the trial store layouts on customer sales.
+
+Task 3 : Analytics and commercial application
+
+Use your analytics and insights from Task 1 and 2 
+to prepare a report for your client, the Category Manager.
+
+"
+getwd()
